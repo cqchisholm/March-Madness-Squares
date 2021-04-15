71eb6db20cf9b8e7c4615d9a5310fc50912af261
cqchisholm <chizjr@gmail.com>
2021-04-15 13:57:41 -0500

    Highlighting row and column now working

--- MMSquares/Squares/static/squares/script.js
+++ MMSquares/Squares/static/squares/script.js
@@ -1,16 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     // All rows and columns as a variable
-    cols = document.querySelectorAll('.col3');
+    cols = document.querySelectorAll('.pool-table td');
         
     // When hovering over
     function on() {
-        cols.forEach(col => {
+        // Get class name of <td> that the user is hovering over
+        var cn = this.className;
+        // Select all <td>'s that have that class name (the entire column)
+        var column = document.querySelectorAll(`.${cn}`);
+        // Change background color of highlighted column
+        column.forEach(col => {
             col.style.backgroundColor = 'rgba(190, 192, 189, 0.534)';
         })
     }
 
-    // When not hovering over
+    // When not hovering over leave column with original background color
     function off() {
         cols.forEach(col => {
             col.style.backgroundColor = '';
